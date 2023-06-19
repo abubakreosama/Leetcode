d1a0b30d7e9e15b540543bbacc222c87c6f0769c
Abubakre <bakry.osama9@gmail.com>
2023-06-20 01:38:35 +0300

    Reverse String

---
+++ Solutions/Reverse String/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        char temp;
+        while (i < j) {
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
